225d026bbe26bfc97810b0d40f27188ba07d2d1c
Hesselt17 <hesselt17@gmail.com>
2024-12-26 21:08:59 -0600

    Initial Commit

---
+++ Return to Sender/Return_to_SenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Return_to_SenderApp.swift
+//  Return to Sender
+//
+//  Created by Tommy Hessel on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Return_to_SenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Return to SenderTests/Return_to_SenderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Return_to_SenderTests.swift
+//  Return to SenderTests
+//
+//  Created by Tommy Hessel on 12/26/24.
+//
+
+import Testing
+@testable import Return_to_Sender
+
+struct Return_to_SenderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
